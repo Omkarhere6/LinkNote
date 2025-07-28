a0e297f4f664a135b45b2f444f3c78c6c4c097b7
Omkar Hawaldar <o.hawaldar@bm-systems.in>
2025-07-28 17:20:49 +0530

    Sign up end point completed

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { ContentModel, LinkModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
+import cors from "cors";
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
+
+        res.json({
+            message: "User signed up"
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
+})
+
+
+
+app.listen(3000);
